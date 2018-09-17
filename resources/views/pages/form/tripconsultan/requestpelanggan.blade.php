232f1d8e4c1fe0d382c863422988c0ae7605f025
BayDeveloper <bayu.webdeveloper@gmail.com>
2018-09-17 16:27:23 -0700

    dual text

--- resources/views/pages/form/tripconsultan/requestpelanggan.blade.php
+++ resources/views/pages/form/tripconsultan/requestpelanggan.blade.php
@@ -27,7 +27,12 @@
       <div class="form-group row">
         <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
         <div class="col-sm-10">
-          <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+          <div class="col">
+            <input type="text" class="form-control" placeholder="First name">
+          </div>
+          <div class="col">
+            <input type="text" class="form-control" placeholder="Last name">
+          </div>
         </div>
       </div>
       <div class="form-group row">
