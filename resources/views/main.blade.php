671e2ba5d5d2af417764d0ef2f287431a3a350e2
BayDeveloper <bayu.webdeveloper@gmail.com>
2018-09-17 15:12:25 -0700

    nav fix 1

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -57,112 +57,33 @@
               <a class="dropdown-item" href="#">Forum</a>
             </div>
           </li>
+
           <li class="nav-item active dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Department
             </a>
-            <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
-                 <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">
-                  <a  class="dropdown-item" tabindex="-1" href="#">Trip Consultant</a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">Trip Consultan</a>
+                <ul class="dropdown-menu">
+                    <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">Form Consultan</a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="tcnewtrip">Form Request Pelanggan</a></li>
+                      <li><a class="dropdown-item" href="#">Another Form..</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">Jon Progress</a>
                   <ul class="dropdown-menu">
-                     <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">
-                      <a class="dropdown-item" href="#">Form Consultant</a>
-                      <ul class="dropdown-menu">
-                          <li class="dropdown-item"><a href="tcnewtrip">Request Pelanggan</a></li>
-                          <li class="dropdown-item"><a href="#">Request ...</a></li>
-                      </ul>
-                    </li>
-                     <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">
-                      <a class="dropdown-item" href="#">Job Progress</a>
-                      <ul class="dropdown-menu">
-                          <li class="dropdown-item"><a href="tcnewtrip">List Progress</a></li>
-                          <li class="dropdown-item"><a href="tcnewtrip">New Progress</a></li>
-                          <li class="dropdown-item"><a href="tcnewtrip">On Progress</a></li>
-                          <li class="dropdown-item"><a href="tcnewtrip">Success</a></li>
-                      </ul>
-                    </li>
-                    <li class="dropdown-item"><a href="#">Other ...</a></li>
+                    <li class="dropdown-item"><a href="#">List Progress</a></li>
+                    <li class="dropdown-item"><a href="#">New Progress</a></li>
+                    <li class="dropdown-item"><a href="#">On Progress</a></li>
+                    <li class="dropdown-item"><a href="#">Success</a></li>
                   </ul>
                 </li>
-                 <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">
-                  <a  class="dropdown-item" tabindex="-1" href="#">Trip Designer</a>
-                  <ul class="dropdown-menu">
-                     <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">
-                      <a class="dropdown-item" href="#">Form Designer</a>
-                      <ul class="dropdown-menu">
-                          <li class="dropdown-item"><a href="#">Request Product</a></li>
-                          <li class="dropdown-item"><a href="#">Request ...</a></li>
-                      </ul>
-                    </li>
-                    <li class="dropdown-item"><a href="#">Other ...</a></li>
-                  </ul>
-                </li>
-                 <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">
-                  <a  class="dropdown-item" tabindex="-1" href="#">Trip Mate</a>
-                  <ul class="dropdown-menu">
-                     <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">
-                      <a class="dropdown-item" href="#">Form Mate</a>
-                      <ul class="dropdown-menu">
-                          <li class="dropdown-item"><a href="#">Request Utility</a></li>
-                          <li class="dropdown-item"><a href="#">Request ...</a></li>
-                      </ul>
-                    </li>
-                    <li class="dropdown-item"><a href="#">Other ...</a></li>
-                  </ul>
-                </li>
-                 <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">
-                  <a  class="dropdown-item" tabindex="-1" href="#">Development</a>
-                  <ul class="dropdown-menu">
-                     <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">
-                      <a class="dropdown-item" href="#">Form Development</a>
-                      <ul class="dropdown-menu">
-                          <li class="dropdown-item"><a href="#">From Survey</a></li>
-                          <li class="dropdown-item"><a href="#">Request ...</a></li>
-                      </ul>
-                    </li>
-                    <li class="dropdown-item"><a href="#">Other ...</a></li>
-                  </ul>
-                </li>
-                 <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">
-                  <a  class="dropdown-item" tabindex="-1" href="#">Trip Promotion</a>
-                  <ul class="dropdown-menu">
-                     <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">
-                      <a class="dropdown-item" href="#">Form Proposal</a>
-                      <ul class="dropdown-menu">
-                          <li class="dropdown-item"><a href="#">Proposal</a></li>
-                          <li class="dropdown-item"><a href="#">Request ...</a></li>
-                      </ul>
-                    </li>
-                    <li class="dropdown-item"><a href="#">Other ...</a></li>
-                  </ul>
-                </li>
-                <li class="dropdown-divider"></li>
-                 <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">
-                  <a  class="dropdown-item" tabindex="-1" href="#">Some other action</a>
-                  <ul class="dropdown-menu">
-                    <li class="dropdown-item"><a tabindex="-1" href="#">Second level</a></li>
-                     <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">
-                      <a class="dropdown-item" href="#">Even More..</a>
-                      <ul class="dropdown-menu">
-                          <li class="dropdown-item"><a href="#">3rd level</a></li>
-                             <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#"><a class="dropdown-item" href="#">another level</a>
-                            <ul class="dropdown-menu">
-                                <li class="dropdown-item"><a href="#">4th level</a></li>
-                                <li class="dropdown-item"><a href="#">4th level</a></li>
-                                <li class="dropdown-item"><a href="#">4th level</a></li>
-                            </ul>
-                          </li>
-                            <li class="dropdown-item"><a href="#">3rd level</a></li>
-                      </ul>
-                    </li>
-                    <li class="dropdown-item"><a href="#">Second level</a></li>
-                    <li class="dropdown-item"><a href="#">Second level</a></li>
-                  </ul>
-                </li>
-              </ul>
-          </li>
 
-          
+                </ul>
+              </li>
+            </ul>
+          </li>
         </ul>
       </div>
     </nav>
